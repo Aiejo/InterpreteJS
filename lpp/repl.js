f268cef935ecb3d2c0b8997a6a7f0623e9c044ce
AnaCGM <anac.99@hotmail.com>
2023-08-22 13:29:00 -0500

    repl

---
+++ lpp/repl.js
@@ -0,0 +1,33 @@
+const Lexer = require('./lexer');
+
+function startRepl(){
+    const linea = require('readline');
+    const rl = linea.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.setPrompt('>>');
+    rl.on('line', (source)=> {
+        
+        if (source !== 'salir'){
+            const lexer = new Lexer(source);
+
+            let token = lexer.siguiente_token();
+
+            while((token.get_literal()) != ''){
+                console.log(token.toString());
+                token = lexer.siguiente_token();
+            }
+        }else{
+            rl.close();
+        }
+
+        rl.prompt();
+                
+    });
+
+    rl.prompt();
+
+}
+startRepl();
